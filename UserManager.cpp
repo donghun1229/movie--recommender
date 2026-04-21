5f00a8eef40b6f9d721046fc8c6cc6017f39bde8
kdh <greatkdh107@gmail.com>
2026-04-21 23:54:16 +0900

    UserManager 기능들 구현

---
+++ UserManager.cpp
@@ -0,0 +1,30 @@
+#include "UserManager.h"
+#include <iostream>
+
+UserManager::UserManager() {
+}
+
+void UserManager::addUser(const User& user) {
+    users.push_back(user);
+}
+
+User* UserManager::findByName(const std::string& name) {
+    for (int i = 0; i < users.size(); i++) {
+        if (users[i].getName() == name) {
+            return &users[i];
+        }
+    }
+
+    return nullptr;
+}
+
+void UserManager::printAll() const {
+    if (users.empty()) {
+        std::cout << "등록된 사용자가 없습니다." << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < users.size(); i++) {
+        users[i].display();
+    }
+}
---
+++ UserManager.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "User.h"
+
+class UserManager {
+private:
+    std::vector<User> users;
+
+public:
+    UserManager();
+
+    void addUser(const User& user);
+    User* findByName(const std::string& name);
+    void printAll() const;
+};
